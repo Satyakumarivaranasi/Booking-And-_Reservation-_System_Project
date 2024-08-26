a0678693c6e3a4accfe2c5fe6c18fc41e0981998
Satyakumarivaranasi <158685085+Satyakumarivaranasi@users.noreply.github.com>
2024-08-26 19:44:36 +0530

    Add files via upload
    my code

---
+++ BookingandReservationSystemHibernate/src/main/java/com/satya/BookingandReservationSystemHibernate/Payments.java
@@ -0,0 +1,74 @@
+package com.satya.BookingandReservationSystemHibernate;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Payments")
+public class Payments {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int paymentID;
+
+    @ManyToOne
+    @JoinColumn(name = "BookingID")
+    private Booking booking;
+
+    @Column(name = "Amount")
+    private BigDecimal amount;
+
+    @Column(name = "PaymentDate")
+    private Date paymentDate;
+
+    @Column(name = "PaymentMethod")
+    private String paymentMethod;
+
+    // Getters and Setters
+    public int getPaymentID() {
+        return paymentID;
+    }
+
+    public void setPaymentID(int paymentID) {
+        this.paymentID = paymentID;
+    }
+
+    public Booking getBooking() {
+        return booking;
+    }
+
+    public void setBooking(Booking booking) {
+        this.booking = booking;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(Date paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+}
---
+++ BookingandReservationSystemHibernate/src/main/java/com/satya/BookingandReservationSystemHibernate/Review.java
@@ -0,0 +1,75 @@
+package com.satya.BookingandReservationSystemHibernate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Review")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int reviewID;
+
+    @ManyToOne
+    @JoinColumn(name = "UserID")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "ServiceID")
+    private Service service;
+
+    @Column(name = "Rating")
+    private int rating;
+
+    @Column(name = "Comment")
+    private String comment;
+    
+    // Getters and Setters
+	public int getReviewID() {
+		return reviewID;
+	}
+
+	public void setReviewID(int reviewID) {
+		this.reviewID = reviewID;
+	}
+
+	public User getuser() {
+		return user;
+	}
+
+	public void setuser(User user) {
+		this.user = user;
+	}
+
+	public Service getService() {
+		return service;
+	}
+
+	public void setService(Service service) {
+		this.service = service;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+   
+}
